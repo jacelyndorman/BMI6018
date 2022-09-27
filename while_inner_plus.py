ef530b7a56eb07074ea0458f2f71e86549c0749c
jacelyndorman <112044564+jacelyndorman@users.noreply.github.com>
2022-09-26 20:46:51 -0600

    Update while_inner_plus.py

--- while_inner_plus.py
+++ while_inner_plus.py
@@ -1,1 +1,22 @@
 
+#Write a python program that, given an input list of any level of complexity/nestedness, will return the inner most list plus 1. 
+#This is to be done with a while loop. Note: the input will contain only integers or lists. 
+#As an example:
+#input_list = [1,2,3,4,[5,6,7,[8,9]]]
+#your_py_program.py input_list will produce:
+#[9,10]
+#That is [8, 9] (the inner most list) plus 1 -> [9, 10]
+
+
+def while_inner(input):   #This is definitely not finished. It does not work yet.
+    empty = []
+    totalitems = 0
+    for item in input:
+        totalitems += 1
+        if isinstance(item, list):           
+            empty.append(item)
+    return(empty)
+            
+    
+while_inner([1,2,3,4,[5,6,7,[8,9]]])
+    

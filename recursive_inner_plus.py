6d187806552732f7a81e40853fc21c73198cde5b
jacelyndorman <112044564+jacelyndorman@users.noreply.github.com>
2022-09-26 20:56:35 -0600

    Update recursive_inner_plus.py

--- recursive_inner_plus.py
+++ recursive_inner_plus.py
@@ -1,1 +1,17 @@
 
+#Write the a python program that, given an input list of any level of complexity/nestedness, will return the inner most list plus 1. 
+#This is to be done with recursion. Note: the input will contain only integers or lists.
+
+
+def recursive_inner(input_list):    #I think this works for what we need. It doesn't work on more complex lists, like ([1,2,3,4,[5,6,7,11],12,[13,14,[15,16]]])
+    empty = []                      #Feel free to let me know if you find another solution, though!
+    for item in input_list:
+        if isinstance(item, list):
+            return(recursive_inner(item))
+        else:
+            item +=1
+            empty.append(item)
+    return(empty)
+
+
+recursive_inner([1,2,3,4,[5,6,7,[8,9]]])

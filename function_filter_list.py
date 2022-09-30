3f1e35ee70ae6c8e0fe8ca02e48230cd641f5151
jacelyndorman <112044564+jacelyndorman@users.noreply.github.com>
2022-09-29 22:14:31 -0600

    Update function_filter_list.py

--- function_filter_list.py
+++ function_filter_list.py
@@ -5,8 +5,6 @@
 #That is, given a list [1,2,3,4,5,6,7,8,9], and an argument (6), it should return [1,2,3,4,5,6]
 
 
-#I wasn't sure I understood this question, so I coded two functions. 
-#This function filters out list values above user defined value.
 
 def function_filter(input_list, input_number):
     newlist = []
@@ -16,21 +14,3 @@ def function_filter(input_list, input_number):
     return(newlist)
 
 function_filter([1,2,3,4,5,6,7,8,9], 6)
-
-
-
-#Question 3
-
-#This function filters list to a user defined length.
-#def function_filter(input_list, input_number):
-#    newlist = []
-#    numberlist = []
-#    variable = 0
-#    for i in range(0,input_number):
-#        numberlist.append(variable)
-#        variable += 1
-#    for eachnumber in numberlist:
-#        newlist.append(input_list[eachnumber])
-#    return(newlist)
-    
-#function_filter([1,2,3,4,5,6,7,8,9], 6)
